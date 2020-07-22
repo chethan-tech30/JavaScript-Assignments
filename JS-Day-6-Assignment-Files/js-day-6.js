755bae20fef87bc38874d972f97ddb4f71eb74f7
Chethan M <47246054+chethan-tech30@users.noreply.github.com>
2020-07-22 11:49:35 +0530

    Assignment Submitted
    JavaScript Day-6 Assignment Submitted

---
+++ JS-Day-6-Assignment-Files/js-day-6.js
@@ -0,0 +1,54 @@
+//Question 1:
+//Solution
+
+function backgroundColorChange() {
+    var colorArray = ["gold", "chartreuse", "blueviolet", "bisque", "darkorange", "darksalmon", "dimgrey", "fuchsia", "hotpink", "powderblue"];
+    var colorIndex = 0;
+    function changePageColor() {
+        document.body.style.backgroundColor = colorArray[colorIndex];
+        colorIndex = (colorIndex + 1) % (colorArray.length);
+        setTimeout(changePageColor, 5000);
+    }
+    changePageColor();
+}
+backgroundColorChange();
+
+// Question 2:
+// Solution
+
+function multiplicationTables() {
+    var num = prompt("Enter the number to print multiplication table: ")
+    num = Number(num);
+
+    for (var i = 1; i <= 10; i++) {
+        document.getElementById('tables').innerHTML += (`${num} x ${i} = ${num * i}<br>`);
+    }
+
+}
+multiplicationTables();
+
+//Question 3:
+//Solution
+
+
+let name = prompt("Enter you Name to print welcome message: ");
+let displayName = document.getElementById('name');
+displayName.innerHTML += ` ${name} Welcome to LetsUpgrade Free JavaScript Coding School`;
+
+const clockTime = document.getElementById('time');
+
+function displayClock() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    clockTime.innerText = time;
+}
+
+setInterval(displayClock, 1000);
+
+function changeMode() {
+    var darkMode = document.body;
+    darkMode.classList.toggle('dark');
+}
+
+
+
